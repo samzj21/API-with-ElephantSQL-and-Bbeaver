422ee73621f19e4398e1a1ec97d6b8c19a70e647
samzj21 <samuelz.0421@gmail.com>
2023-05-16 19:22:22 -0500

    API with ElephantSQL and Bbeaver

---
+++ src/domain/entities/Teacher.ts
@@ -0,0 +1,15 @@
+export class Teacher {
+
+    constructor(
+
+        public name: string,
+        public description: string,
+        public email: string,
+        public birthDate: Date,
+        public id?: string
+
+    ) {
+
+    }
+
+} 
